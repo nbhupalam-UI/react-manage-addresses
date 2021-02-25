d166d05ccc89d0768e60052c23cadf5380ccbd8e
Bhupalam, Naga Pradeep (US - Hyderabad) <nbhupalam@deloitte.com>
2021-02-25 18:47:21 +0530

    Removed usage of aux file

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -1,16 +1,11 @@
 import React from "react";
 
-import Aux from "../../../hoc/Aux/Aux";
 import NavigationItem from "./NavigationItem/NavigationItem";
 import classes from "./NavigationItems.module.scss";
 
 const navigationItems = ({ isAuthenticated }) => (
   <ul className={classes.NavigationItems}>
-    {isAuthenticated && (
-      <Aux>
-        <NavigationItem link="/logout">Logout</NavigationItem>
-      </Aux>
-    )}
+    {isAuthenticated && <NavigationItem link="/logout">Logout</NavigationItem>}
   </ul>
 );
 
